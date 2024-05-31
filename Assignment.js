6b1ec2158c0b9ff42590bc70376a09d662ea0b10
MD MAHABUB HASAN <162690116+Mahabub-bd-com@users.noreply.github.com>
2024-05-31 21:12:05 +0600

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,105 @@
+//Create a variable called carName, assign the value Volvo to it.
+//Question 01
+let carName = "Volvo" ; 
+console.log(carName)
+
+//On one single line, declare three variables with the following names and values:
+//Question 02
+firstName = "John"
+lastName = "Doe"
+age = 35
+
+// So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+// 3rd Variable Name And Values Both! 
+//Question 03
+let firstName = "John";  // write variable name
+let lastName = "Doe" //write variable values 
+let age = 35 ; // write variable name and values both 
+
+// Use the correct assignment operator that will 
+// result in x being 50 (same as x = x * y). 
+//Question 04
+let x = 10;
+let y = 5; // what will be the value of x ? 
+
+// Use comments to describe the correct data type of the following variables:
+//Question 05
+let length = 16; // data type ? 
+let LastName = "Johnson"; // data type ? 
+
+const x = {
+  firstName: "John",  
+  lastName: "Doe"
+};    
+string // data type ? 
+
+//Question 06
+Execute the function named myFunction.
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+// Hello World! // call the function and see the output 
+
+//Question 07
+// Create an object called person with name = John, age = 50, Then,
+access the object to alert("John is 50").
+function myFunction() {
+    alert("John is 50");
+  }
+
+// The <button> element should do something when someone clicks on it. Try to fix it!
+//Question 08
+<button>Click me.</button> 
+// Click me
+
+// Alert the number of items in an array, using the correct Array property: 
+const cars = ["Volvo", "Jeep", "Mercedes"];
+
+//Change the first item of Brand to "Ford".
+const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+
+console.log(cars.replace("Volvo", "Jeep", "Mercedes", "Volvo", "Jeep", "Mercedes"))
+
+//Question 09
+//Use the correct Math method to create a random number.
+<p>Math.random() returns a random number between 0 (inclusive) and 1 (exclusive):</p>
+<p id="demo"></p>
+<script>
+document.getElementById("demo").innerHTML = Math.random();
+</script>
+
+//Use the correct Math method to return the largest number of 10 and 20.
+<p>Return the number with the highest value:</p>
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = Math.max(5, 10);
+</script>
+
+//Use the correct Math method to get the square root of 9.
+<p>Math.sqrt() returns the square root of a number:</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = Math.sqrt(9);
+</script>
+
+//Question 10
+//Choose the correct comparison operator to alert true, when x is greater than y. 
+x = 10;
+y = 5;
+
+alert(x > y) // ? 
+
+//Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+
+
+
+
+
+
+
+
+
